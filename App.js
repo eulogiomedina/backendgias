bcc4b3ef4051eb415c5d00ea4d3acfad74022d12
eulogiomedina <134175832+eulogiomedina@users.noreply.github.com>
2024-10-24 23:57:28 -0600

    Update App.js

--- App.js
+++ App.js
@@ -88,7 +88,7 @@ app.post('/api/auth/logout', (req, res) => {
 });
 
 // Escuchar en el puerto configurado
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
