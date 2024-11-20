16d0f8eedad41ae47eff8a0d32b8db4054d6f40b
jesuscruz12 <134993609+jesuscruz12@users.noreply.github.com>
2024-11-19 22:10:42 -0600

    Update terms.js

--- routes/terms.js
+++ routes/terms.js
@@ -14,7 +14,10 @@ router.get('/:id', termController.getTermById);
 // Actualizar un término por ID
 router.put('/:id', termController.updateTerm);
 
-// Eliminar un término por ID
-router.delete('/:id', termController.deleteTerm);
+// Eliminar un término por ID (eliminación lógica)
+router.delete('/delete/:id', termController.softDeleteTerm);
+
+// Restaurar un término eliminado
+router.put('/restore/:id', termController.restoreTerm);
 
 module.exports = router;
